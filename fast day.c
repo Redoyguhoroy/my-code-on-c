ff9661395427d8b8c56777d1ee0249fd029c6d7c
Redoyguhoroy <107880682+Redoyguhoroy@users.noreply.github.com>
2022-06-20 23:58:20 +0600

    Create fast day.c

---
+++ fast day.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    //Input two Value and perform all arithmetic operations.
+    int a,b;
+    scanf("%d%d",&a,&b);
+    printf("%d\n",a+b);
+    printf("%d\n",a-b);
+    printf("%d\n",a*b);
+    printf("%d\n",a/b);
+    printf("%d\n",a%b);
+    return 0;
+}
